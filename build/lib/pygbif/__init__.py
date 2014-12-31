c549eb37676236eeb3440a99d6c9e9ef176d727e
Scott Chamberlain <myrmecocystus@gmail.com>
2014-12-30 23:36:20 -0600

    working minimal version

---
+++ build/lib/pygbif/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+# pygbif
+
+'''
+pygbif library
+~~~~~~~~~~~~~~~~~~~~~
+
+pygbif is a Python client for GBIF. Example usage:
+
+   >>> import pygbif
+   >>> pygbif.xxxx
+'''
+
+from .search import *
