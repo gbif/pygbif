014135b192c16dc0ccfc6c0c531217c3d81cd66f
Scott Chamberlain <myrmecocystus@gmail.com>
2015-10-27 17:37:22 -0700

    bump dev version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pygbif',
-	version='0.0.1',
+	version='0.0.2',
 	description='Python client for GBIF',
     author='Scott Chamberlain',
     author_email='myrmecocystus@gmail.com',
