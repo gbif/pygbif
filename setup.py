9e3aa84720cd954a6e041d3d7235367bd2000ea4
Scott Chamberlain <myrmecocystus@gmail.com>
2015-11-03 22:33:14 -0800

    added changes file, think i fixed setup.py. #8

--- setup.py
+++ setup.py
@@ -1,17 +1,27 @@
+import codecs
 from setuptools import setup
 from setuptools import find_packages
 
-setup(name='pygbif',
-	version='0.1.0',
-	description='Python client for GBIF',
-  author='Scott Chamberlain',
-  author_email='myrmecocystus@gmail.com',
-  url='http://github.com/sckott/pygbif',
-  license="MIT",
-  # packages=['pygbif'],
-  packages = find_packages(exclude=['test-*']),
-  install_requires=['requests>2.7'],
-  classifiers=(
+with codecs.open('README.md', 'r', 'utf-8') as f:
+    readme = f.read()
+
+with codecs.open('CHANGES.txt', 'r', 'utf-8') as f:
+    changes = f.read()
+
+long_description = readme + '\n\n' + changes
+
+setup(
+  name             = 'pygbif',
+	version          = '0.1.1',
+	description      = 'Python client for GBIF',
+  long_description = long_description,
+  author           = 'Scott Chamberlain',
+  author_email     = 'myrmecocystus@gmail.com',
+  url              = 'http://github.com/sckott/pygbif',
+  license          = "MIT",
+  packages         = find_packages(exclude=['test-*']),
+  install_requires = ['requests>2.7'],
+  classifiers      = (
     'Development Status :: 3 - Alpha',
     'Intended Audience :: Science/Research',
     'Topic :: Scientific/Engineering :: Bio-Informatics',
