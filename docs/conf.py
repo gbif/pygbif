e4fc5e605e16881e059c02f5580580e5d24987d6
Scott Chamberlain <myrmecocystus@gmail.com>
2015-12-04 23:07:35 -0800

    updated docs

--- docs/conf.py
+++ docs/conf.py
@@ -21,6 +21,7 @@ import shlex
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath('..'))
 
+import pygbif
 from pygbif import occurrences
 
 # -- General configuration ------------------------------------------------
