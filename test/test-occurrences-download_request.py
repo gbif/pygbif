fff4032ab86e22e340df7804f24bb7c457a6d25f
stijnvanhoey <stijnvanhoey@gmail.com>
2016-06-07 10:16:18 +0200

    add tests for predicate building

--- test/test-occurrences-download_request.py
+++ test/test-occurrences-download_request.py
@@ -1,11 +1,9 @@
-
 import unittest
 
 from pygbif.occurrences.download import GbifDownload
 
 
 class TestGbifClass(unittest.TestCase):
-
     def test_gbif_creation(self):
         """test the creation of the predicate class"""
 
@@ -35,16 +33,49 @@ class TestGbifClass(unittest.TestCase):
         req.add_predicate("COUNTRY", "BE", "equals")
         self.assertIsInstance(req.payload["predicate"]["predicates"], list)
         self.assertEquals(len(req.payload["predicate"]["predicates"]), 1)
-        # TODO
+        self.assertIsInstance(req.payload["predicate"]["predicates"][0], dict)
+        self.assertDictEqual(req.payload["predicate"]["predicates"][0],
+                             {'key': 'COUNTRY',
+                              'type': 'equals',
+                              'value': 'BE'})
+
+    def test_add_iterative_predicate(self):
+        """the the predicate addition of an iterative sequence"""
+        req = GbifDownload("name", "email")
+        req.add_iterative_predicate("TAXONKEY", [3189866, 2498252])
+        self.assertIsInstance(req.payload["predicate"]["predicates"], list)
+        self.assertEquals(len(req.payload["predicate"]["predicates"]), 1)
+        self.assertIsInstance(req.payload["predicate"]["predicates"][0], dict)
+
+        temp_pred = req.payload["predicate"]["predicates"][0]["predicates"]
+        self.assertIsInstance(temp_pred, list)
+        self.assertEquals(len(temp_pred), 2)
+        self.assertIsInstance(temp_pred[0], dict)
+
+        self.assertEquals(set(list(temp_pred[0].keys())),
+                             set(['key', 'type', 'value']))
+        self.assertEquals(req.payload["predicate"]["predicates"][0]['type'],
+                          'or')
+
+    def test_add_geometry(self):
+        """check predicate after adding a geometry"""
+        req = GbifDownload("name", "email")
+        req.add_geometry(
+            'POLYGON((-14.06 42.55, 9.84 38.27, -7.03 26.43, -14.06 42.55))')
+        self.assertIsInstance(req.payload["predicate"]["predicates"], list)
+        self.assertEquals(len(req.payload["predicate"]["predicates"]), 1)
+        self.assertEquals(
+            set(req.payload["predicate"]["predicates"][0].keys()),
+            set(['type', 'geometry']))
 
     def test_lookup_predicates(self):
         """different lookups checked"""
+        # main combination predicate
         req = GbifDownload("name", "email")
         req.main_pred_type = "|"
         self.assertEqual(req.payload['predicate']['type'], "or")
 
-        # ADAPT -> hier voor een gewoon predicate doen!
-        req.main_pred_type = ">="
-        self.assertEqual(req.payload['predicate']['type'],
+        # predicate addition
+        req.add_predicate("YEAR", "2000", ">=")
+        self.assertEqual(req.payload["predicate"]["predicates"][0]["type"],
                          "greaterThanOrEquals")
-
