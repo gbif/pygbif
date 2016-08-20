32c52a976bfc0ede3c767bd5939c9f6e7fd84bfc
Scott Chamberlain <myrmecocystus@gmail.com>
2016-08-20 08:28:26 -0400

    fix get test

--- test/test-occurrences-get.py
+++ test/test-occurrences-get.py
@@ -4,17 +4,17 @@ from pygbif import occurrences
 
 def test_get():
     "occurrences.get - basic test"
-    res = occurrences.get(key = 252408386)
+    res = occurrences.get(key = 1248387548)
     assert 'dict' == res.__class__.__name__
     assert len(res) > 30
-    assert 252408386 == res['key']
+    assert 1248387548 == res['key']
 
 def test_get_verbatim():
     "occurrences.get_verbatim - basic test"
-    res = occurrences.get_verbatim(key = 252408386)
+    res = occurrences.get_verbatim(key = 1248387548)
     assert 'dict' == res.__class__.__name__
-    assert 24 == len(res)
-    assert 252408386 == res['key']
+    assert len(res) > 20
+    assert 1248387548 == res['key']
 
 def test_get_fragment():
     "occurrences.get_fragment - basic test"
