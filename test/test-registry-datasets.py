019d7e38e06e4018e67f6f838926ff5f81d0c539
Scott Chamberlain <myrmecocystus@gmail.com>
2016-10-19 14:43:15 -0700

    fix some datasets tests

--- test/test-registry-datasets.py
+++ test/test-registry-datasets.py
@@ -11,18 +11,18 @@ def test_datasets_limit():
     "registry.datasets - limit param"
     res = registry.datasets(limit=1)
     assert dict == res.__class__
-    assert 1 == len(res['data'])
+    assert 1 == len(res['results'])
 
     res = registry.datasets(limit=3)
     assert dict == res.__class__
-    assert 3 == len(res['data'])
+    assert 3 == len(res['results'])
 
 def test_datasets_type():
     "registry.datasets - type param"
     res = registry.datasets(type="OCCURRENCE")
-    vv = [ x['type'] for x in res['data'] ]
+    vv = [ x['type'] for x in res['results'] ]
     assert dict == res.__class__
-    assert 100 == len(res['data'])
+    assert 100 == len(res['results'])
     assert 'OCCURRENCE' == list(set(vv))[0]
 
 def test_dataset_metrics():
