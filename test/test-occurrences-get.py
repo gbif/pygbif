98156e368d195a56a6b05949306efffdcc9c7ac6
Scott Chamberlain <myrmecocystus@gmail.com>
2017-04-27 08:08:28 -0700

    fix occurrences.get_fragment test

--- test/test-occurrences-get.py
+++ test/test-occurrences-get.py
@@ -20,5 +20,4 @@ def test_get_fragment():
     "occurrences.get_fragment - basic test"
     res = occurrences.get_fragment(key = 1052909293)
     assert 'dict' == res.__class__.__name__
-    assert 41 == len(res)
-    assert 'HumanObservation' == res['basisOfRecord']
+    assert 'humanobservation' == res['basisOfRecord']
