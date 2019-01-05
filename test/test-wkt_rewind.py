83c50b9c5a63e7e490de92b498aeb7a1143e97cf
Scott Chamberlain <myrmecocystus@gmail.com>
2019-01-04 19:31:39 -0800

    fix tst

--- test/test-wkt_rewind.py
+++ test/test-wkt_rewind.py
@@ -1,5 +1,6 @@
 """Tests for utils module - wkt_rewind"""
 from nose.tools import *
+import unittest
 import re
 from pygbif import utils
 
@@ -10,12 +11,13 @@ def test_wkt_rewind():
     res = utils.wkt_rewind(x)
     assert str == res.__class__
 
-def test_wkt_rewind_digits():
-    "utils.wkt_rewind - digits works as expected"
-    res = utils.wkt_rewind(x, digits = 3)
-    pat = re.compile('\\.[0-9]{3}\\s')
-    out = pat.search(res)
-    assert re.Match == out.__class__
+class TestWKTClass(unittest.TestCase):
+    def test_wkt_rewind_digits(self):
+        "utils.wkt_rewind - digits works as expected"
+        res = utils.wkt_rewind(x, digits = 3)
+        pat = re.compile('\\.[0-9]{3}\\s')
+        out = pat.search(res)
+        self.assertIsInstance(out, re.Match)
 
 @raises(TypeError)
 def test_wkt_rewind_fails_well():
