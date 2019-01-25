237cc577d7364253421764b9c94b4b2891509e13
Scott Chamberlain <myrmecocystus@gmail.com>
2019-01-25 10:16:50 -0800

    fix #48 fix typo in installations add missing tests for remainder of registry modules: installations, networks, organizations bump version

---
+++ test/test-registry-networks.py
@@ -0,0 +1,29 @@
+"""Tests for registry module - networks"""
+import os
+import vcr
+from pygbif import registry
+
+@vcr.use_cassette('test/vcr_cassettes/test_networks.yaml')
+def test_networks():
+    "registry.networks - basic test"
+    res = registry.networks()
+    assert dict == res.__class__
+    assert 2 == len(res)
+    assert 1 == len(res['data'])
+    assert ['data', 'meta'] == sorted(res.keys())
+
+@vcr.use_cassette('test/vcr_cassettes/test_networks_limit.yaml')
+def test_networks_limit():
+    "registry.networks - limit param "
+    res = registry.networks(limit=5)
+    assert dict == res.__class__
+    assert 1 == len(res['data'])
+
+@vcr.use_cassette('test/vcr_cassettes/test_networks_uuid.yaml')
+def test_networks_uuid():
+    "registry.networks - with a uuid"
+    res = registry.networks(uuid="2b7c7b4f-4d4f-40d3-94de-c28b6fa054a6")
+    assert dict == res.__class__
+    assert 2 == len(res)
+    assert str == res['data']['title'].__class__
+    assert 'modifiedBy' in res['data'].keys()
