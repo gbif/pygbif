e61bbf710d5543c354e8821d409433bdb0e2b87a
Nicolas Noé <nicolas.noe@inbo.be>
2020-05-26 10:19:52 +0200

    More Docstring + black formatting

--- setup.py
+++ setup.py
@@ -3,49 +3,50 @@ import re
 from setuptools import setup
 from setuptools import find_packages
 
-version = ''
-with open('pygbif/__init__.py', 'r') as fd:
-    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
-                        fd.read(), re.MULTILINE).group(1)
+version = ""
+with open("pygbif/__init__.py", "r") as fd:
+    version = re.search(
+        r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', fd.read(), re.MULTILINE
+    ).group(1)
 
 if not version:
-    raise RuntimeError('Cannot find version information')
+    raise RuntimeError("Cannot find version information")
 
-with codecs.open('README.rst', 'r', 'utf-8') as f:
+with codecs.open("README.rst", "r", "utf-8") as f:
     readme = f.read()
 
-with codecs.open('Changelog.rst', 'r', 'utf-8') as f:
+with codecs.open("Changelog.rst", "r", "utf-8") as f:
     changes = f.read()
-changes = changes.replace(':issue:', '')
-long_description = readme + '\n\n' + changes
+changes = changes.replace(":issue:", "")
+long_description = readme + "\n\n" + changes
 
 setup(
-  name             = 'pygbif',
-	version          = version,
-	description      = 'Python client for GBIF',
-  long_description = long_description,
-  author           = 'Scott Chamberlain',
-  author_email     = 'myrmecocystus@gmail.com',
-  url              = 'http://github.com/sckott/pygbif',
-  license          = "MIT",
-  packages         = find_packages(exclude=['test-*']),
-  install_requires = [
-    'requests>2.7', 
-    'requests-cache',
-    'geojson_rewind', 
-    'geomet',
-    'appdirs>=1.4.3',
-    'matplotlib'
-  ],
-  classifiers      = [
-    'Development Status :: 4 - Beta',
-    'Intended Audience :: Science/Research',
-    'Topic :: Scientific/Engineering :: Bio-Informatics',
-    'Natural Language :: English',
-    'License :: OSI Approved :: MIT License',
-    'Programming Language :: Python',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
-    'Programming Language :: Python :: 3.7'
-	]
+    name="pygbif",
+    version=version,
+    description="Python client for GBIF",
+    long_description=long_description,
+    author="Scott Chamberlain",
+    author_email="myrmecocystus@gmail.com",
+    url="http://github.com/sckott/pygbif",
+    license="MIT",
+    packages=find_packages(exclude=["test-*"]),
+    install_requires=[
+        "requests>2.7",
+        "requests-cache",
+        "geojson_rewind",
+        "geomet",
+        "appdirs>=1.4.3",
+        "matplotlib",
+    ],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+        "Natural Language :: English",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
 )
