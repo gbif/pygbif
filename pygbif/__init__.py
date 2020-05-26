dcad63faf0248ebda958a167058bfcd40a41c3db
Nicolas Noé <nicolas.noe@inbo.be>
2020-05-26 10:18:59 +0200

    More Docstring + black formatting

--- pygbif/__init__.py
+++ pygbif/__init__.py
@@ -10,30 +10,30 @@ pygbif is a Python client for the Global Biodiversity Information Facility (GBIF
 
 Usage::
 
-		# Import entire library
-		import pygbif
-		# or import modules as needed
-		## occurrences
-		from pygbif import occurrences
-		## species
-		from pygbif import species
-		## registry
-		from pygbif import registry
+        # Import entire library
+        import pygbif
+        # or import modules as needed
+        ## occurrences
+        from pygbif import occurrences
+        ## species
+        from pygbif import species
+        ## registry
+        from pygbif import registry
 
-		## use advanced logging
-		### setup first
-		import requests
-		import logging
-		import http.client
-		http.client.HTTPConnection.debuglevel = 1
-		logging.basicConfig()
-		logging.getLogger().setLevel(logging.DEBUG)
-		requests_log = logging.getLogger("requests.packages.urllib3")
-		requests_log.setLevel(logging.DEBUG)
-		requests_log.propagate = True
-		### then make request
-		from pygbif import occurrences
-		occurrences.search(geometry='POLYGON((30.1 10.1, 10 20, 20 40, 40 40, 30.1 10.1))', limit=20)
+        ## use advanced logging
+        ### setup first
+        import requests
+        import logging
+        import http.client
+        http.client.HTTPConnection.debuglevel = 1
+        logging.basicConfig()
+        logging.getLogger().setLevel(logging.DEBUG)
+        requests_log = logging.getLogger("requests.packages.urllib3")
+        requests_log.setLevel(logging.DEBUG)
+        requests_log.propagate = True
+        ### then make request
+        from pygbif import occurrences
+        occurrences.search(geometry='POLYGON((30.1 10.1, 10 20, 20 40, 40 40, 30.1 10.1))', limit=20)
 """
 
 __version__ = "0.4.0"
