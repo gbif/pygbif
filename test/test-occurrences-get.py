60a07393149c3f05deca75510dbbea87a8439850
John Waller <jhnwllr@gmail.com>
2024-05-02 13:58:19 +0200

    updating requests_cache requirments to 1.0.0 #147

--- test/test-occurrences-get.py
+++ test/test-occurrences-get.py
@@ -6,19 +6,19 @@ from pygbif import occurrences
 @vcr.use_cassette("test/vcr_cassettes/test_get.yaml")
 def test_get():
     "occurrences.get - basic test"
-    res = occurrences.get(key=1986620884)
+    res = occurrences.get(key=142316497)
     assert "dict" == res.__class__.__name__
     assert len(res) > 30
-    assert 1986620884 == res["key"]
+    assert 142316497 == res["key"]
 
 
 @vcr.use_cassette("test/vcr_cassettes/test_get_verbatim.yaml")
 def test_get_verbatim():
     "occurrences.get_verbatim - basic test"
-    res = occurrences.get_verbatim(key=1986620884)
+    res = occurrences.get_verbatim(key=142316497)
     assert "dict" == res.__class__.__name__
     assert len(res) > 20
-    assert 1986620884 == res["key"]
+    assert 142316497 == res["key"]
 
 
 @vcr.use_cassette("test/vcr_cassettes/test_get_fragment.yaml")
