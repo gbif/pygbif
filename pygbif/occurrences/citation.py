49fd7c46ec3d89b219a75df5a1bdbccad43a5142
John Waller <ftw712@ku.dk>
2024-10-02 10:25:39 +0200

    adding error checking to citation

--- pygbif/occurrences/citation.py
+++ pygbif/occurrences/citation.py
@@ -1,4 +1,5 @@
 from pygbif.gbifutils import gbif_baseurl, gbif_GET_raw
+import re
 
 def citation(key):
     """
@@ -15,6 +16,9 @@ def citation(key):
 
     """
     url = gbif_baseurl + "occurrence/download/" + str(key) + "/citation"
-    out = gbif_GET_raw(url).decode('utf-8')
-    return(out)
+    if re.fullmatch(r'^\d+-\d+$', key):
+        out = gbif_GET_raw(url).decode('utf-8')
+        return(out)
+    else:
+        raise ValueError("key must be a GBIF download key")
 
