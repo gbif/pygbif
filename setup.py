13ca6790ec6c427a0fa146730f094b2111bb94d5
jhnwllr <jhnwllr@gmail.com>
2025-11-14 14:09:44 +0100

    fixing build

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     author="Scott Chamberlain",
     author_email="myrmecocystus@gmail.com",
     url="http://github.com/gbif/pygbif",
-    download_url="https://github.com/gbif/pygbif/archive/refs/tags/v0.6.5.tar.gz",
+    download_url="https://github.com/gbif/pygbif/archive/refs/tags/v0.6.6.tar.gz",
     license="MIT",
     packages=find_packages(exclude=["test-*"]),
     install_requires=[
@@ -43,7 +43,8 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11"
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12"
     ],
     keywords = ['gbif', 'biodiversity', 'specimens', 'API', 'web-services', 'occurrences', 'species', 'taxonomy'],
 )
