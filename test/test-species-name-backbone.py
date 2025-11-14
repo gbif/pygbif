8299d7ce90e48fe2d26d05f572b84848a5ab24fd
jhnwllr <jhnwllr@gmail.com>
2025-11-14 13:23:15 +0100

    fixing tests and fresh cassettes

--- test/test-species-name-backbone.py
+++ test/test-species-name-backbone.py
@@ -8,9 +8,9 @@ def test_name_backbone():
     "species.name_backbone - basic test"
     res = species.name_backbone(scientificName="Calopteryx splendens")
     assert dict == res.__class__
-    assert 7 == len(res)
+    assert 5 == len(res)
     assert "Calopteryx splendens (Harris, 1780)" == res["usage"]["name"]
-    assert list(res.keys()) == ['usage', 'classification', 'diagnostics', 'additionalStatus', 'synonym', 'left', 'right']
+    assert list(res.keys()) == ['usage', 'classification', 'diagnostics', 'additionalStatus', 'synonym']
 
 @vcr.use_cassette("test/vcr_cassettes/test_name_backbone_verbose.yaml")
 def test_name_backbone_verbose():
@@ -26,9 +26,9 @@ def test_name_backbone_class():
     "species.name_backbone - class test"
     res = species.name_backbone(class_="Insecta")
     assert dict == res.__class__
-    assert 6 == len(res)
+    assert 4 == len(res)
     assert "Insecta" == res["usage"]["name"]
-    assert list(res.keys()) == ['usage', 'classification', 'diagnostics', 'synonym', 'left', 'right']
+    assert list(res.keys()) == ['usage', 'classification', 'diagnostics', 'synonym']
 
 @vcr.use_cassette("test/vcr_cassettes/test_name_backbone_checklistKey.yaml")
 def test_name_backbone_checklistKey():
@@ -36,7 +36,7 @@ def test_name_backbone_checklistKey():
     res = species.name_backbone(scientificName="Calopteryx splendens", checklistKey="7ddf754f-d193-4cc9-b351-99906754a03b")
     assert dict == res.__class__
     assert res["usage"]["key"] == "Q2M4"
-    assert 7 == len(res)
+    assert 5 == len(res)
     assert "Calopteryx splendens" == res["usage"]["canonicalName"]
-    assert list(res.keys()) == ['usage', 'classification', 'diagnostics', 'additionalStatus', 'synonym', 'left', 'right']
+    assert list(res.keys()) == ['usage', 'classification', 'diagnostics', 'additionalStatus', 'synonym']
 
