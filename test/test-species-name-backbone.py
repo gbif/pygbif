d2fe6fcb75c630c7069817cdc461d72b59c8d718
jhnwllr <jhnwllr@gmail.com>
2026-08-21 14:39:05 +0200

    Set COL Extended Release as default for name_backbone()

--- test/test-species-name-backbone.py
+++ test/test-species-name-backbone.py
@@ -5,12 +5,13 @@ from pygbif import species
 
 @vcr.use_cassette("test/vcr_cassettes/test_name_backbone.yaml")
 def test_name_backbone():
-    "species.name_backbone - basic test"
+    "species.name_backbone - basic test (COL Extended Release default)"
     res = species.name_backbone(scientificName="Calopteryx splendens")
     assert dict == res.__class__
-    assert 7 == len(res)
+    assert 5 == len(res)
     assert "Calopteryx splendens (Harris, 1780)" == res["usage"]["name"]
-    assert list(res.keys()) == ['usage', 'classification', 'diagnostics', 'additionalStatus', 'synonym', 'left', 'right']
+    assert res["usage"]["key"] == "Q2M4"  # COL key
+    assert list(res.keys()) == ['usage', 'classification', 'diagnostics', 'additionalStatus', 'synonym']
 
 @vcr.use_cassette("test/vcr_cassettes/test_name_backbone_verbose.yaml")
 def test_name_backbone_verbose():
@@ -23,12 +24,32 @@ def test_name_backbone_verbose():
 
 @vcr.use_cassette("test/vcr_cassettes/test_name_backbone_class.yaml")
 def test_name_backbone_class():
-    "species.name_backbone - class test"
+    "species.name_backbone - class test (COL Extended Release default)"
     res = species.name_backbone(class_="Insecta")
     assert dict == res.__class__
-    assert 6 == len(res)
+    assert 4 == len(res)
     assert "Insecta" == res["usage"]["name"]
-    assert list(res.keys()) == ['usage', 'classification', 'diagnostics', 'synonym', 'left', 'right']
+    assert res["usage"]["key"] == "H6"  # COL key
+    assert list(res.keys()) == ['usage', 'classification', 'diagnostics', 'synonym']
+
+@vcr.use_cassette("test/vcr_cassettes/test_name_backbone_gbif.yaml")
+def test_name_backbone_gbif_backbone():
+    "species.name_backbone - explicit GBIF Backbone (checklistKey=None)"
+    res = species.name_backbone(scientificName="Calopteryx splendens", checklistKey=None)
+    assert dict == res.__class__
+    assert "Calopteryx splendens (Harris, 1780)" == res["usage"]["name"]
+    assert isinstance(res["usage"]["key"], str)
+    assert res["usage"]["key"].isdigit()  # GBIF Backbone uses numeric keys
+
+@vcr.use_cassette("test/vcr_cassettes/test_name_backbone_gbif_uuid.yaml")
+def test_name_backbone_gbif_backbone_uuid():
+    "species.name_backbone - explicit GBIF Backbone UUID"
+    res = species.name_backbone(scientificName="Calopteryx splendens", checklistKey="d7dddbf4-2cf0-4f39-9b2a-bb099caae36c")
+    assert dict == res.__class__
+    assert "Calopteryx splendens (Harris, 1780)" == res["usage"]["name"]
+    assert res["usage"]["key"] == "1427067"  # GBIF Backbone numeric key
+    assert isinstance(res["usage"]["key"], str)
+    assert res["usage"]["key"].isdigit()
 
 @vcr.use_cassette("test/vcr_cassettes/test_name_backbone_checklistKey.yaml")
 def test_name_backbone_checklistKey():
