e9febc0a53d609dd07012dd75864981498bdbb38
jhnwllr <jhnwllr@gmail.com>
2026-08-26 10:25:45 +0200

    Update test_download_cancel to work in CI and regenerate cassette

--- test/test-occurrences-download_cancel.py
+++ test/test-occurrences-download_cancel.py
@@ -1,13 +1,17 @@
-"""Tests for occurrences module - download_cancle methods"""
+"""Tests for occurrences module - download_cancel methods"""
 from pygbif import occurrences as occ
 import vcr
 import os
 import pytest
 
-IN_GITHUB_ACTIONS = os.getenv("GITHUB_ACTIONS") == "true"
-print(IN_GITHUB_ACTIONS)
+# Skip if credentials not available (they are provided in CI via secrets)
+SKIP_TEST = not all([
+    os.getenv("GBIF_USER"),
+    os.getenv("GBIF_PWD"), 
+    os.getenv("GBIF_EMAIL")
+])
 
-@pytest.mark.skipif(IN_GITHUB_ACTIONS, reason="Test doesn't work well in Github Actions.")
+@pytest.mark.skipif(SKIP_TEST, reason="Test requires GBIF credentials")
 @vcr.use_cassette("test/vcr_cassettes/test_download_cancel.yaml", filter_headers=["authorization"])
 def test_download_cancel():
     "occurrences.download_cancel - basic test"
